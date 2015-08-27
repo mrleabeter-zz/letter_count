f9a2c271689b39b660ce61041302e70da1c8bac5
Christopher Leabeter <christopherleabeter@gmail.com>
2015-08-26 21:54:30 -0400

    Method that returns list of characters along with their indeces in a string.

--- index.rb
+++ index.rb
@@ -1,18 +1,20 @@
-#Write a method 'count_letters' that can tell us all the unique characters that exist in a string that is passed into the method. Furthermore, the method should also report back how many instances of each letter were found in the string.
+#Write a method 'count_letters' that can tell us all the unique characters that exist in a string that is passed into the method. The method should also return all the indices in the string where each character is found.
 
-def count_letter(string)
+def letter_index(string)
   characters = {}
-  characters.default = 0
-  string.downcase!.split('').each do |count|
-    characters[count] += 1
+  string.split('').each_with_index do |letter, location|
+      if characters[letter] ==  nil
+        characters[letter] = [location]
+      else
+        characters[letter] << location 
+    end
   end
-  puts "Here is a list of the characters that were in the string you provided, and the number of times each occured within the string:"
-  characters. each do |character_key, occurence_value|
-    puts "#{character_key.inspect}: #{occurence_value}"
+  puts "Here is a list of the characters that were in the string you provided, along with the indices where each occured within the string:"
+    characters.each do |letter, location|
+    puts "#{letter.inspect}: #{location}"
   end
 end
 
-teststring1 = "Today is Wednesday, August the 26th."
-#teststring2 = "Tomorrow is Thursday, August the 27th."
+teststring1 = "lighthouse in the house..."
 
-count_letter(teststring1)
+letter_index(teststring1)
