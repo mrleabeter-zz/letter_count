8365515fbd91590dc6c8df9f814665937b05df3c
Christopher Leabeter <christopherleabeter@gmail.com>
2015-08-26 20:07:11 -0400

    Created a method which lists each unique character for a string, and how many times each character occurs.

---
+++ count.rb
@@ -0,0 +1,27 @@
+#Write a method 'count_letters' that can tell us all the unique characters that exist in a string that is passed into the method. Furthermore, the method should also report back how many instances of each letter were found in the string.
+
+# characters.each do |key, value|
+#     print key
+
+teststring1 = "Today is Wednesday, August the 26th."
+#teststring2 = "Tomorrow is Thursday, August the 27th."
+
+def count_letter(string)
+  characters = {}
+  characters.default = 0
+  string.downcase!
+  string.split('').each do |block_var|
+    characters[block_var] += 1
+  end
+  # characters.each do |character_key, occurence_value|
+    
+  # end
+  puts "Here is a list of the characters that were in the string you provided, and the number of times each occured within the string:"
+  characters. each do |character_key, occurence_value|
+    puts "#{character_key}: #{occurence_value}"
+  end
+end
+
+
+
+count_letter(teststring1)
